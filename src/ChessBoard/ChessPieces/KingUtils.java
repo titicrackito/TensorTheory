cce596bd2a12a29c1f8bc02f1d485ac16f3157a3
titi_crackito <titouanhemeury@gmail.com>
2026-07-03 00:50:47 +0200

    started to add king legal move

--- src/ChessBoard/ChessPieces/KingUtils.java
+++ src/ChessBoard/ChessPieces/KingUtils.java
@@ -3,10 +3,35 @@ package ChessBoard.ChessPieces;
 import ChessBoard.Board;
 import ChessBoard.MovePiece;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class KingUtils {
+
+    private static final long COLUMN_A = 0x0101010101010101L;
+    private static final long COLUMN_H = 0x8080808080808080L;
+
+    private static final long[] KING_ATTACKS = new long[64];
+
+    static {
+        for (int square = 0; square < 64; square++) {
+            long king = 1L << square;
+            long moves = 0L;
+
+            moves |= king << 8;
+            moves |= king >>> 8;
+
+            if ((king & COLUMN_H) == 0) {
+                moves |= king << 1;
+                moves |= king << 9;
+                moves |= king >>> 7;
+            }
+            if ((king & COLUMN_A) == 0) {
+                moves |= king >>> 1;
+                moves |= king >>> 9;
+                moves |= king << 7;
+            }
+            KING_ATTACKS[square] = moves;
+        }
+    }
+
     public static boolean IsMate(Board board) {
         if (board.whiteKing == 0L) {
             System.out.println("Black won by checkmate");
@@ -18,19 +43,23 @@ public class KingUtils {
         }
         return false;
     }
+
     public static boolean KingLegalMove(Board board, long kingToMove, long coordinate) {
-        if (MovePiece.IsSquareOccupied(board, kingToMove, coordinate)){return false;}
-        final long COLUMN_A = 0x0101010101010101L;
-        final long COLUMN_H = 0x8080808080808080L;
-        List<Integer> shift = new ArrayList<>();
-        shift.addAll(List.of(1,7,8,9));
-        if (kingToMove != (kingToMove & ~COLUMN_A)) {shift.removeAll(List.of(-9,-1,7));}
-        if (kingToMove != (kingToMove & ~COLUMN_H)) {shift.removeAll(List.of(9,1,-7));}
-        for (int index:shift) {
-            if ((kingToMove<<index==coordinate) || (kingToMove<<((-1)*index)==coordinate)) {
-                return true;
-            }
+        int square = Long.numberOfTrailingZeros(kingToMove);
+        if ((KING_ATTACKS[square] & coordinate) == 0) {
+            return false;
         }
+        if (MovePiece.IsSquareOccupied(board, kingToMove, coordinate)) {
+            return false;
+        }
+        return true;
+    }
+    public static boolean IsSquareAttackedWhite(Board board, long squareToCheck){
+        if (RookUtils.RookLegalMoves(board,squareToCheck,board.getBlackRooks())) {return true;}
+        if (BishopUtils.BishopLegalMoves(board,squareToCheck,board.getBlackBishops())) {return true;}
+        if (KnightUtils.KnightLegalMoves(board,squareToCheck,board.getBlackKnights())) {return true;}
+        if (QueenUtils.QueenLegalMoves(board,squareToCheck,board.getBlackQueens())) {return true;}
+        if ((((squareToCheck<<7) & board.getBlackPawns())!=0) || (((squareToCheck>>>9) & board.getBlackPawns())!=0)){return true;}
         return false;
     }
 }
